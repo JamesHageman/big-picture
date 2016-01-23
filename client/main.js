221559460cefb8d70bf418c80bca4aed469d40aa
Daniel Prilik <danielprilik@gmail.com>
2016-01-22 23:13:23 -0500

    Look! Stuff is happening!

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 (function () {
     //------------------------- CONSTS ------------------------//
-    var DIMS = 64;
-    var SIZE = 16;
+    var DIMS = 32;
+    var SIZE = 24;
 
     var GW = DIMS*SIZE;
     var GH = DIMS*SIZE;
@@ -13,15 +13,155 @@
         pX = 0,
         pY = 0,
         gScale = 1,
-        speed = 2;
+        speed = 1;
+
+    var grid_on = true;
 
     var isDown = false;
 
+    var drawMode = false;
+
+
+    var picture = [];
+
+    var colors = ["white","lightblue","pink","lightgreen","black"];
+
     //------------------------- LOGICZ ------------------------//
     
-    function genGrid() {
-        
+    function genEmptyPicture() {
+        for (var r = 0; r < DIMS; ++r) {
+            picture.push([]);
+            for (var c = 0; c < DIMS; ++c) {
+                picture[r].push( ((Math.random()<0.5)?0:((Math.random()<0.5)?1:((Math.random()<0.5)?2:((Math.random()<0.5)?3:4)))) );
+            }
+        }
     } 
+
+    //------------------------- RENDER ------------------------//
+
+    function zoomPicture(delta) {
+        gScale += delta * 0.01;
+        if (gScale < 1) gScale = 1;
+        renderPicture();
+    }
+
+    function rescalePicture(e) {
+        gX -= (pX - e.pageX) * speed;
+        gY -= (pY - e.pageY) * speed;
+        pX = e.pageX;
+        pY = e.pageY;
+        if (gX > 0) gX = 0;
+        if (gX < canvas.width - GW * gScale) gX = canvas.width - GW * gScale;
+        if (gY > 0) gY = 0;
+        if (gY < canvas.height - GH * gScale) gY = canvas.height - GH * gScale;
+
+        renderPicture();
+    }
+
+    function renderPicture() {
+        // clear the old canvasaroo
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.save();
+        ctx.translate(gX, gY);
+        ctx.scale(gScale, gScale);
+
+        for (var r = 0; r < DIMS; ++r) {
+            for (var c = 0; c < DIMS; ++c) {
+                var lOffset = c * SIZE;
+                var tOffset = r * SIZE;
+
+                ctx.fillStyle = colors[picture[r][c]];
+                ctx.fillRect(lOffset, tOffset, SIZE, SIZE);
+
+                if (grid_on) {
+                    ctx.strokeStyle = "rgba(100,100,100,0.5)";
+                    ctx.lineWidth=0.5;
+                    ctx.strokeRect(lOffset, tOffset, SIZE, SIZE);
+                }
+            }
+        }
+        ctx.restore();
+    }
+
+    //--------------------- EVENT HANDLERS --------------------//
+
+    $('#drawingboard').mousedown(function (e) {
+        isDown = true;
+        
+        // Move Mode
+        if (!drawMode) {
+            pX = e.pageX;
+            pY = e.pageY;
+        }
+
+        // Draw Mode
+        if (drawMode) {
+
+        }
+
+    }).mouseup(function (e) {
+        isDown = false;
+    }).mouseout(function (e) {
+        isDown = false;
+    }).mousemove(function (e) {
+        // Move Mode
+        if (!drawMode) {
+            if (isDown) rescalePicture(e);
+        }
+
+        // Draw Mode
+        if (drawMode) {
+
+        }
+    });
+
+    // Mousewheel
+    function wheel(event) {
+        // Move Mode
+        if (!drawMode){
+            var delta = 0;
+            if (!event) event = window.event;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+            } else if (event.detail) {
+                delta = -event.detail / 3;
+            }
+            if (delta) {
+                zoomPicture(delta);
+            }
+            if (event.preventDefault) {
+                event.preventDefault();
+            }
+            event.returnValue = false;
+        }
+
+        // Draw Mode
+        if (drawMode) {
+
+        }
+    }
+
+    if (window.addEventListener) {
+        window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
+
+
+    // Buttons
+    $("#btn_draw").click(function(){
+        drawMode = true;
+        console.log("draw mode on");
+    });
+    $("#btn_move").click(function(){
+        drawMode = false;
+        console.log("draw mode off");
+    });
+    $("#btn_toggle_grid").click(function(){
+        grid_on = !grid_on;
+        console.log("grid toggled " + ((grid_on)?"on":"off"));
+        renderPicture();
+    });
 
     //-------------------------- MAIN -------------------------//
 
@@ -29,101 +169,12 @@
     var canvas = document.getElementById('drawingboard');
     var ctx = canvas.getContext("2d");
 
-    ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 1;
-    ctx.font = "14px sans-serif";
+    canvas.width = DIMS*SIZE;
+    canvas.height = DIMS*SIZE;
 
-    var grid = [];
-    for (var i = 0; i < cells; ++i) {
-        if (Math.random() < 0.5) {
-            grid.push("#FF8ED6");
-        } else {
-            grid.push("#8ED6FF");
-        }
-    }
+    genEmptyPicture(DIMS);
 
-    drawGrid(0, 0);
-
-    $('#canvas').mousedown(function (e) {
-        isDown = true;
-        pX = e.pageX;
-        pY = e.pageY;
-    }).mouseup(function (e) {
-        isDown = false;
-    }).mouseout(function (e) {
-        isDown = false;
-    }).mousemove(function (e) {
-        if (isDown) {
-            gX += (pX - e.pageX) * speed;
-            gY += (pY - e.pageY) * speed;
-            pX = e.pageX;
-            pY = e.pageY;
-            if (gX > 0) gX = 0;
-            if (gX < canvas.width - gW * gScale) gX = canvas.width - gW * gScale;
-            if (gY > 0) gY = 0;
-            if (gY < canvas.height - gH * gScale) gY = canvas.height - gH * gScale;
-
-            drawGrid();
-        }
-    });
-
-    function drawGrid() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        ctx.save();
-        ctx.translate(gX, gY);
-        ctx.scale(gScale, gScale);
-        for (var i = 0; i < cols; ++i) {
-            ctx.fillStyle = "#FFFFFF";
-            ctx.fillRect(i * size, 0, size, headerSize);
-            ctx.strokeRect(i * size, 0, size, headerSize);
-
-            ctx.fillStyle = "#000000";
-            ctx.save();
-            ctx.translate(i * size + 16, headerSize - 8);
-            ctx.rotate(Math.PI * 1.5);
-            ctx.fillText("Column " + i, 0, 0);
-            ctx.restore();
-
-            for (var j = 0; j < rows; ++j) {
-                ctx.fillStyle = grid[i * rows + j];
-                ctx.fillRect(i * size, j * size + headerSize, size, size);
-                ctx.strokeRect(i * size, j * size + headerSize, size, size);
-            }
-        }
-        ctx.restore();
-    }
-
-    /*
-     * Mousewheel
-     */
-    function handle(delta) {
-        gScale += delta * 0.01;
-        if (gScale < 1) gScale = 1;
-        drawGrid();
-    }
-
-    function wheel(event) {
-        var delta = 0;
-        if (!event) event = window.event;
-        if (event.wheelDelta) {
-            delta = event.wheelDelta / 120;
-        } else if (event.detail) {
-            delta = -event.detail / 3;
-        }
-        if (delta) {
-            handle(delta);
-        }
-        if (event.preventDefault) {
-            event.preventDefault();
-        }
-        event.returnValue = false;
-    }
-
-    if (window.addEventListener) {
-        window.addEventListener('DOMMouseScroll', wheel, false);
-    }
-    window.onmousewheel = document.onmousewheel = wheel;
+    renderPicture();
 
     console.log("gethype");
 })();
