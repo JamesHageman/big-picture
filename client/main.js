455173482890e043a0c68f6850340935c420aca5
Daniel Prilik <danielprilik@gmail.com>
2016-01-23 00:06:24 -0500

    tweking well

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-(function () {
+// (function () {
     //------------------------- CONSTS ------------------------//
     var DIMS = 32;
     var SIZE = 24;
@@ -25,6 +25,7 @@
     var picture = [];
 
     var colors = ["white","lightblue","pink","lightgreen","black"];
+    var selectedColor = 4;
 
     //------------------------- LOGICZ ------------------------//
     
@@ -32,10 +33,36 @@
         for (var r = 0; r < DIMS; ++r) {
             picture.push([]);
             for (var c = 0; c < DIMS; ++c) {
-                picture[r].push( ((Math.random()<0.5)?0:((Math.random()<0.5)?1:((Math.random()<0.5)?2:((Math.random()<0.5)?3:4)))) );
+                random = false;
+
+                if (random) picture[r].push( ((Math.random()<0.5)?0:((Math.random()<0.5)?1:((Math.random()<0.5)?2:((Math.random()<0.5)?3:4)))) );
+                else picture[r].push(0);
             }
         }
     } 
+    
+    /* returns {r:row, c:column} given mouse position of canvas */
+    function resolveClickedPictureElement(cords) {
+        eR = cords.y;
+        eC = cords.x;
+
+        // translate
+        eR -= gY;
+        eC -= gX;
+
+        // scale
+        eR /= gScale;
+        eC /= gScale;
+
+        // get cords
+        eR = Math.floor(eR / SIZE);
+        eC = Math.floor(eC / SIZE);
+
+        return {
+            r:eR,
+            c:eC
+        };
+    }
 
     //------------------------- RENDER ------------------------//
 
@@ -45,7 +72,7 @@
         renderPicture();
     }
 
-    function rescalePicture(e) {
+    function panPicture(e) {
         gX -= (pX - e.pageX) * speed;
         gY -= (pY - e.pageY) * speed;
         pX = e.pageX;
@@ -72,7 +99,7 @@
                 var tOffset = r * SIZE;
 
                 ctx.fillStyle = colors[picture[r][c]];
-                ctx.fillRect(lOffset, tOffset, SIZE, SIZE);
+                ctx.fillRect(lOffset, tOffset, SIZE+1, SIZE+1); // +1 cuz zoom
 
                 if (grid_on) {
                     ctx.strokeStyle = "rgba(100,100,100,0.5)";
@@ -82,6 +109,16 @@
             }
         }
         ctx.restore();
+    }
+
+    function initColorButtons() {
+        for (var i = 0; i < colors.length; i++) {
+            var colorButton = $('<input type="button" value="' + colors[i] + '" id = "btn_' + i + '"/>');
+            $("#colorContainer").append(colorButton);
+            $("#btn_"+i).click(function() {
+                selectedColor = parseInt(this.id.replace( /^\D+/g, ''));
+            })
+        }
     }
 
     //--------------------- EVENT HANDLERS --------------------//
@@ -97,7 +134,12 @@
 
         // Draw Mode
         if (drawMode) {
+            var tile = resolveClickedPictureElement(canvas.relMouseCoords(e));
 
+            if (picture[tile.r][tile.c] != selectedColor) {
+                picture[tile.r][tile.c] = selectedColor;
+                renderPicture();
+            }
         }
 
     }).mouseup(function (e) {
@@ -107,12 +149,17 @@
     }).mousemove(function (e) {
         // Move Mode
         if (!drawMode) {
-            if (isDown) rescalePicture(e);
+            if (isDown) panPicture(e);
         }
 
         // Draw Mode
-        if (drawMode) {
+        if (drawMode && isDown) {
+            var tile = resolveClickedPictureElement(canvas.relMouseCoords(e));
 
+            if (picture[tile.r][tile.c] != selectedColor) {
+                picture[tile.r][tile.c] = selectedColor;
+                renderPicture();
+            }
         }
     });
 
@@ -163,6 +210,28 @@
         renderPicture();
     });
 
+    //------------------------- MAGIC -------------------------//
+
+    function relMouseCoords(event){
+        var totalOffsetX = 0;
+        var totalOffsetY = 0;
+        var canvasX = 0;
+        var canvasY = 0;
+        var currentElement = this;
+
+        do{
+            totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+            totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+        }
+        while(currentElement = currentElement.offsetParent)
+
+        canvasX = event.pageX - totalOffsetX;
+        canvasY = event.pageY - totalOffsetY;
+
+        return {x:canvasX, y:canvasY}
+    }
+    HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
     //-------------------------- MAIN -------------------------//
 
     // Init canvas context
@@ -172,9 +241,14 @@
     canvas.width = DIMS*SIZE;
     canvas.height = DIMS*SIZE;
 
+    // Init color buttons
+    initColorButtons();
+
+    // generate / load image
     genEmptyPicture(DIMS);
 
+    // Inital render
     renderPicture();
 
     console.log("gethype");
-})();
+// })();
