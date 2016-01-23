110aa51182278f8604e80284f1070b9f0a62bdef
James Hageman <jamesd@hageman.ca>
2016-01-22 21:48:01 -0500

    Basic image crop

---
+++ server/crop.js
@@ -0,0 +1,21 @@
+import Canvas, { Image } from 'canvas'
+
+export default function crop({
+  buf,
+  x,
+  y,
+  size,
+  cb
+}) {
+  const canvas = new Canvas(size, size);
+  const ctx = canvas.getContext('2d');
+  const img = new Image;
+
+  img.src = buf;
+
+  ctx.drawImage(img, -x, -y, img.width, img.width);
+
+  canvas.toBuffer((err, newBuf) => {
+    cb(err, newBuf);
+  })
+}
