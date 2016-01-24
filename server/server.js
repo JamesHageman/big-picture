34006a43f12b597a06de16b2ec3a1a492db19203
James Hageman <jamesd@hageman.ca>
2016-01-23 23:14:59 -0500

    fix port again

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('babel-register');
 
 var app = require('./app').default;
 
-var PORT = 8080;//process.env.NODE_ENV === 'production' ? 80 : 8080;
+var PORT = process.env.PORT || 8080;
 
 app.listen(PORT, function () {
   console.log('Server running on port ' + PORT);
