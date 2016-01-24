bb5839d0eef2688c3632d0e52622222e647690ab
James Hageman <jamesd@hageman.ca>
2016-01-23 22:47:31 -0500

    8080

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('babel-register');
 
 var app = require('./app').default;
 
-var PORT = process.env.NODE_ENV === 'production' ? 80 : 8080;
+var PORT = 8080;//process.env.NODE_ENV === 'production' ? 80 : 8080;
 
 app.listen(PORT, function () {
   console.log('Server running on port ' + PORT);
