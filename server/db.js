7b6cc91337030e741b16d92bde6e10e2612f3831
James Hageman <jamesd@hageman.ca>
2016-01-28 20:29:42 -0500

    Remove db

--- server/db.js
+++ server/db.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose'
 
-export const url = process.env.NODE_ENV === 'production' ?
-  'mongodb://james:master@dbh15.mongolab.com:27157/big-picture' :
-  'mongodb://localhost/big-picture'
+export const url = process.env.DB_URL || 'mongodb://localhost/big-picture'
 
 mongoose.connect(url)
 
